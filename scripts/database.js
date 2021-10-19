3eb740f5e1da9e363148d9003597ae5ef0b8fae9
jkuns76 <jkoonman@gmail.com>
2021-10-19 15:37:41 -0500

    donr for the day

--- scripts/database.js
+++ scripts/database.js
@@ -34,7 +34,24 @@ const database = {
             timestamp: 1614659931693
         }
     ],
-    orderBuilder: {},
+    orderBuilder: {
+        id: 1,
+        metalId: 1,
+        styleId: 1,
+        sizeId: 1,
+        timestamp: 1
+    }
+}
+export const setMetal = (id) => {
+    database.orderBuilder.metalId = id
+}
+
+export const setSize = (id) => {
+    database.orderBuilder.sizeId = id
+}
+
+export const setStyle = (id) => {
+    database.orderBuilder.styleId = id
 }
 
 export const getMetals = () => {
@@ -52,14 +69,23 @@ export const getStyles = () => {
 export const getOrders = () => {
     return database.customOrders.map(order => ({ ...order }))
 }
-export const setMetal = (id) => {
-    database.orderBuilder.metalId = id
-}
+export const addCustomOrder = () => {
+    // Copy the current state of user choices
+    const newOrder = {...database.orderBuilder}
 
-export const setSize = (id) => {
-    database.orderBuilder.sizeId = id
-}
+    // Add a new primary key to the object
+    const lastIndex = database.customOrders.length - 1
+    newOrder.id = database.customOrders[lastIndex].id + 1
 
-export const setStyle = (id) => {
-    database.orderBuilder.styleId = id
+    // Add a timestamp to the order
+    newOrder.timestamp = Date.now()
+
+    // Add the new order object to custom orders state
+    database.customOrders.push(newOrder)
+
+    // Reset the temporary state for user choices
+    database.orderBuilder = {}
+
+    // Broadcast a notification that permanent state has changed
+    document.dispatchEvent(new CustomEvent("stateChanged"))
 }
